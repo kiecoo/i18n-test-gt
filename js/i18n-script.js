94b415ac8d47dc18c66bdecd10266815126094bd
Michelle <34873832+kiecoo@users.noreply.github.com>
2019-05-19 17:53:40 +0800

    UPDATE 調整成data-i18n選取

--- js/i18n-script.js
+++ js/i18n-script.js
@@ -4,9 +4,38 @@ function selectButton(lang) {
     $('#' + lang).addClass('selected');
 }
 
-function changeLang(lang) {
-    $('h1.title').text(window.I18N[lang].TITLE);
-    $('.partner-container h3').text(window.I18N[lang].PARTNER);
+function changeLang(lang) {    
+    //    *****************   UPDATE data-i18n *************************
+    changeAllText(lang)
+         //$('h1.title').text(window.I18N[lang].TITLE);
+         //$('.partner-container h3').text(window.I18N[lang].PARTNER);
+    // END  *****************   UPDATE data-i18n  ******************************
     selectButton(lang);
     currentLang = lang;
 }
+
+/*****************   UPDATE data-i18n   ******************************
+             (instead  1. lang-en.js  2.lang-zh-tw.js )          *******/
+const i18njson_en = {
+  translate_partner: 'partner:',
+  translate_website: 'website',
+};
+
+const i18njson_jp = {
+  translate_partner: 'パートナー:',
+  translate_website: 'ウェブサイト',
+};
+
+function changeAllText(lang) {
+  Array.from(document.querySelectorAll("[data-i18n]")).forEach(node=>{
+    const key = node.getAttribute("data-i18n");
+    //txt = i18njson[key];
+    if(lang=='en'){
+        txt = i18njson_en[key];
+    }else{
+        txt = i18njson_jp[key];
+    }   
+    node.childNodes[0].nodeValue = txt;
+  });
+}
+// END  *****************   UPDATE data-i18n  ******************************
